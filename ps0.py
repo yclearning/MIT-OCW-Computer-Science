1848f528c0039bc2accf2bd8da5d780dadb83d39
YC <38008530+yclearning@users.noreply.github.com>
2023-10-06 21:22:07 +0100

    Create ps0.py

---
+++ ps0.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 17:05:20 2023
+
+@author: YC
+"""
+
+import math
+#Write a program that does the following in order:
+    
+#1. Asks the user to enter a number “x” 
+
+x_input = input("Please enter a number x: ")
+
+x = float(x_input)
+
+#2. Asks the user to enter a number “y”
+
+y_input = input("Please enter a number y: ")
+
+y = float(y_input)
+
+#3. Prints out number “x”, raised to the power “y”. 
+
+print(x)
+x = x**y
+
+#4. Prints out the log (base 2) of “x”.
+
+log_base_2 = math.log2(x)
+
+print(log_base_2)
